d4974cdfaef89ba1bfbd2a12fca528e4cdf58e6d
Himanshu Raj <hr59871@gmail.com>
2025-05-26 02:07:16 +0530

    added path

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useChatStore } from '../store/useChatStore'
 import Sidebar from '../components/Sidebar'
-import ChatContainer from '../components/chatContainer'
+import ChatContainer from '../components/ChatContainer'
 import NoChatSelected from '../components/NoChatSelected'
 
 
