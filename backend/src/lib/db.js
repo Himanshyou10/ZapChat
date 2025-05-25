6cf7eb74898e39914070886e4c7bf38b349cfe50
Himanshu Raj <hr59871@gmail.com>
2025-05-26 01:19:07 +0530

    project completed

---
+++ backend/src/lib/db.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+export const connectDB = async () => {
+    try {
+        const connection = await mongoose.connect(process.env.MONGODB_URL);
+        console.log(`MongoDB connected: ${connection.connection.host}`);
+    } catch (error) {
+        console.log("MongoDB connection failed:", error);
+        process.exit(1);
+    }
+};
---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,33 @@
+ import express from "express";
+import { checkAuth, login, logout, signup, updateProfile} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { sendMessage } from "../controllers/message.controller.js";
+
+ const router = express.Router();
+
+router.post("/signup", signup );
+
+router.post("/login", login);
+
+router.post("/logout", logout);
+
+router.put("/update-profile",protectRoute, updateProfile); 
+
+router.get("/check", protectRoute, checkAuth);
+
+router.post("/send/:id", protectRoute, sendMessage )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ export default router;
